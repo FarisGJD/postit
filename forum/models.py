b598de5f686bc167c0a3f871b99ef9caa5f9016b
Faris Dhoot <farisgjd@outlook.com>
2022-05-18 22:22:43 +0000

    Fix databse syntax errors and update str method for the categories model

--- forum/models.py
+++ forum/models.py
@@ -11,7 +11,7 @@ class Postit(models.Model):
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="forum_posts")
     generated_on = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
-    link = models.URLField(max_length=200, bull=True, blank=True)
+    link = models.URLField(max_length=200, null=True, blank=True)
     image = CloudinaryField('image', default='placeholder')
     topic = models.CharField(max_length=20, unique=True)
     votes = models.ManyToManyField(User, related_name='post_votes', blank=True)
@@ -31,19 +31,19 @@ class Categories(models.Model):
     '''User generated category model dervied from the Postit model''' 
     category = models.ManyToManyField(Postit, related_name='user_categories', blank=True)
 
-    def list_of_categories(self):
-        return self.category
+    def __str__(self):
+        return str(self.category)
 
-
+        
 class Reply(models.Model):
     '''User reply to commnets'''
-    postit = models.ForeignKey(Postit, on_delete=models.CASCADE, related_name='comments')
+    postit = models.ForeignKey(Postit, on_delete=models.CASCADE, related_name='reply_comments')
     username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reply')
     comment = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ['date_created']
 
     def _str_(self):
         return f"Comment {self.comment} by {self.username}"
@@ -59,7 +59,7 @@ class Comments(models.Model):
     replies = models.ManyToManyField(Reply, blank=True)
 
     class Meta:
-        ordering = ['created_on']
+        ordering = ['date_created']
 
     def _str_(self):
         return f"Comment {self.comment} by {self.username}"
