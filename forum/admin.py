ed0345d3abb0669ad4b8d394dbad46c0c008ba23
Faris Dhoot <farisgjd@outlook.com>
2022-05-20 16:09:55 +0000

    Create and configure admin page with models & General Database tidyup

--- forum/admin.py
+++ forum/admin.py
@@ -1,3 +1,43 @@
 from django.contrib import admin
+from .models import Postit, Comment, Reply, Category
+from django_summernote.admin import SummernoteModelAdmin
 
-# Register your models here.
+
+@admin.register(Postit)
+class PostitAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('heading',)}
+    list_filter = ('generated_on', 'topic')
+    search_fields = ['heading', 'content', 'topic']
+    list_display = ('heading', 'slug', 'generated_on')
+    summernote_fields = ('body')
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+
+    search_fields = ['category']
+
+
+@admin.register(Comment)
+class CommentsAdmin(admin.ModelAdmin):
+
+    list_filter = ('approved', 'date_created')
+    search_fields = ['username', 'comment']
+    list_display = (
+        'username', 'comment', 'postit', 'date_created', 'approved'
+        )
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+@admin.register(Reply)
+class ReplyAdmin(admin.ModelAdmin):
+    list_filter = ['date_created']
+    search_fields = ['username', 'comment']
+    list_display = (
+        'username', 'comment', 'postit', 'date_created'
+        )
+
--- forum/models.py
+++ forum/models.py
@@ -1,6 +1,17 @@
+'''Database'''
+
 from django.db import models
 from django.contrib.auth.models import User
 from cloudinary.models import CloudinaryField
+
+
+class Category(models.Model):
+    '''User generated category model dervied from the Postit model'''
+    category = models.CharField(max_length=50)
+    slug = models.SlugField(max_length=300, unique=True, blank=True)
+    
+    def __str__(self):
+        return str(self.category)
 
 
 class Postit(models.Model):
@@ -8,12 +19,16 @@ class Postit(models.Model):
     thread_starter = models.BooleanField(default=True)
     heading = models.CharField(max_length=300, unique=True)
     slug = models.SlugField(max_length=300, unique=True)
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="forum_posts")
+    author = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="forum_posts"
+        )
     generated_on = models.DateTimeField(auto_now_add=True)
     body = models.TextField()
     link = models.URLField(max_length=200, null=True, blank=True)
     image = CloudinaryField('image', default='placeholder')
-    topic = models.CharField(max_length=20, unique=True)
+    topic = models.ManyToManyField(
+        Category, related_name='generated_category', blank=True
+        )
     votes = models.ManyToManyField(User, related_name='post_votes', blank=True)
 
     class Meta:
@@ -24,43 +39,45 @@ class Postit(models.Model):
         return str(self.heading)
 
     def upvote_count(self):
+        '''Returns number of likes'''
         return self.votes.count()
 
 
-class Categories(models.Model):
-    '''User generated category model dervied from the Postit model''' 
-    category = models.ManyToManyField(Postit, related_name='user_categories', blank=True)
-
-    def __str__(self):
-        return str(self.category)
-
-        
 class Reply(models.Model):
     '''User reply to commnets'''
-    postit = models.ForeignKey(Postit, on_delete=models.CASCADE, related_name='reply_comments')
-    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_reply')
-    comment = models.TextField()
+    postit = models.ForeignKey(
+        Postit, on_delete=models.CASCADE, related_name='reply_comments'
+        )
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_reply'
+        )
+    comments = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
 
     class Meta:
+        '''Organises commnets in asending order'''
         ordering = ['date_created']
 
     def _str_(self):
-        return f"Comment {self.comment} by {self.username}"
+        return f"Comment {self.comments} by {self.username}"
 
 
-class Comments(models.Model):
-    '''User inital comments to post''' 
-    postit = models.ForeignKey(Postit, on_delete=models.CASCADE, related_name='comments')
-    username = models.ForeignKey(User, on_delete=models.CASCADE, related_name='user_comment')
+class Comment(models.Model):
+    '''User inital comments to post'''
+    postit = models.ForeignKey(
+        Postit, on_delete=models.CASCADE, related_name='comments'
+        )
+    username = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name='user_comment'
+        )
     comment = models.TextField()
     date_created = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
     replies = models.ManyToManyField(Reply, blank=True)
 
     class Meta:
+        '''Organises commnets in asending order'''
         ordering = ['date_created']
 
     def _str_(self):
         return f"Comment {self.comment} by {self.username}"
-
