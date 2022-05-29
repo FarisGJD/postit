5de062797cc7b5cef9551e72ec552a71a4bfbc08
Faris Dhoot <farisgjd@outlook.com>
2022-05-29 16:10:29 +0000

    fix cloudinary bug

--- postit/settings.py
+++ postit/settings.py
@@ -27,11 +27,11 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+# DEBUG = False
 
 X_FRAME_OPTIONS = "SAMEORIGIN"
 
-# DEBUG = 'DEVELOPMENT' in os.environ
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['postit-fd.herokuapp.com', 'localhost']
 
