21dc9a060c3975342684a5e46080c4d45f261f34
Faris Dhoot <farisgjd@outlook.com>
2022-05-30 04:11:19 +0000

    Media queries

--- static/js/script.js
+++ static/js/script.js
@@ -1,11 +1,18 @@
-const defensiveButton = document.querySelector(".defensive-button"); 
-const defensiveContainer = document.querySelector("#delete-defensive-container");
+// const defensiveButton = document.querySelector(".defensive-button"); 
+// const defensiveContainer = document.querySelector("#delete-defensive-container");
 
 
-defensiveButton.onclick = function () {
-    defensiveContainer.classList.toggle("active")
-}; 
+// defensiveButton.onclick = function () {
+//     defensiveContainer.classList.toggle("active")
+// }; 
 
-// window.onclick = function () {
-//     defensiveContainer.classList.toggle("remove")
-// }
+const hamburger = document.getElementById('hamburger');
+const navUl = document.getElementById('navbar');
+const ctaButton = document.getElementById('cta-button')
+
+console.log(navUl, hamburger, ctaButton)
+
+hamburger.addEventListener('click', () => {
+    navUl.classList.toggle('show'); 
+    ctaButton.classList.toggle('show');
+})
